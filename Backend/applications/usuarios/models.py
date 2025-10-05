4a730d4683fa579f08d65a1bd947e47d3ef50f28
erwin jaime <erwinnosqui@gmail.com>
2025-10-05 02:13:19 -0500

    ajuste de models.py

--- Backend/applications/usuarios/models.py
+++ Backend/applications/usuarios/models.py
@@ -17,12 +17,19 @@ class Usuario(AbstractBaseUser):
     correo = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
     creadoen = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     
-    # Para que funcione con JWT
     objects = UsuarioManager()
     USERNAME_FIELD = 'correo'
     REQUIRED_FIELDS = ['nombre']
 
     class Meta:
         db_table = "usuarios"
-        managed = True  # Cambiar a True
+        managed = True
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
