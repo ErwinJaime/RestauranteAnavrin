0e39fee1e243cf5f47de7f38e6f098b1076f215e
erwin jaime <erwinnosqui@gmail.com>
2025-10-04 21:33:56 -0500

    Refactor el modelo Usuario para implementar un gestor de usuarios personalizado y mejorar la gestión de contraseñas

--- Backend/applications/usuarios/models.py
+++ Backend/applications/usuarios/models.py
@@ -1,18 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
-class Usuario(models.Model):
+class UsuarioManager(BaseUserManager):
+    def create_user(self, correo, password=None, **extra_fields):
+        if not correo:
+            raise ValueError('El correo es obligatorio')
+        correo = self.normalize_email(correo)
+        user = self.model(correo=correo, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class Usuario(AbstractBaseUser):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=150)
     correo = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
     creadoen = models.DateTimeField(auto_now_add=True)
+    
+    # Para que funcione con JWT
+    objects = UsuarioManager()
+    USERNAME_FIELD = 'correo'
+    REQUIRED_FIELDS = ['nombre']
 
     class Meta:
         db_table = "usuarios"
-        managed = True
-    
-    def set_password(self, raw_password):
-        # Aquí puedes agregar lógica para hashear la contraseña antes de guardarla
-        from django.contrib.auth.hashers import make_password
-        self.password = make_password(raw_password)  # Reemplaza esto con el hash real en producción
+        managed = True  # Cambiar a True
