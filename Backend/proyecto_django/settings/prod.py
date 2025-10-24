ce4162d7d073cb061e1e56646cd1dc8292b94ef1
erwin jaime <erwinnosqui@gmail.com>
2025-10-24 10:57:14 -0500

    Agregar archivo .env para configuración de Google Client ID y actualizar .gitignore para incluir .env.production

--- Backend/proyecto_django/settings/prod.py
+++ Backend/proyecto_django/settings/prod.py
@@ -3,6 +3,7 @@ import dj_database_url
 from .base import *
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID") 
 
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
