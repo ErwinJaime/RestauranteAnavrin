10d27780655aa7fe04ea65f085c632519ce06312
Tatiana Nieto <lnietogoyeneche@gmail.com>
2025-11-08 12:46:02 -0500

    Cambios de iconos

--- FrontEnd/src/views/InventarioPage.vue
+++ FrontEnd/src/views/InventarioPage.vue
@@ -78,16 +78,14 @@
                 <td class="td-opciones text-center">
                   <div class="opciones-container">
                     <button class="btn-editar" @click="editarProducto(producto.id)">
-                     <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                       <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-                       <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                     <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
+                       <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
                      </svg>
                      Editar
                    </button>
                    <button class="btn-eliminar" @click="confirmarEliminarProducto(producto.id)">
-                     <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                       <polyline points="3 6 5 6 21 6"></polyline>
-                       <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                     <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
+                       <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
                      </svg>
                      Eliminar
                    </button>
@@ -719,7 +717,8 @@ export default {
 .producto-img {
   width: 100%;
   height: 100%;
-  object-fit: cover;
+  object-fit: contain;  
+  background-color: white;
 }
 
 /* Columna de nombre */
@@ -797,7 +796,9 @@ export default {
 
 .btn-editar svg,
 .btn-eliminar svg {
-  flex-shrink: 0;  
+  flex-shrink: 0;
+  width: 18px;
+  height: 18px;  
 }
 
 .btn-editar {
