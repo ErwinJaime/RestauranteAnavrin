302e0dea3f6e7c0058323d13646be6d8d7985f52
Tatiana Nieto <lnietogoyeneche@gmail.com>
2025-11-26 13:32:40 -0500

    cambios platillos, postres

--- FrontEnd/src/routers/index.js
+++ FrontEnd/src/routers/index.js
@@ -2,23 +2,25 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from '@/views/LoginForm.vue'
 import Home from '@/views/HomePage.vue'
 import Platillos from '@/views/PlatillosPage.vue'
+import Postres from '@/views/PostresPage.vue'
 import Dashboard from '@/views/UserDashboard.vue'
 import Registro from '@/views/RegisterForm.vue'
 import Administrador from '@/views/InventarioPage.vue'
 import ResenasAdmin from '@/views/ResenasAdmin.vue'
 import About from '@/views/AboutPage.vue'
-import ResenasHome from '@/views/ResenasHome.vue'
+/*import ResenasHome from '@/views/ResenasHome.vue'
 import AboutHome from '@/views/AboutPageHome.vue'
 import Menu from '@/views/MenuPage.vue'
 import ResenasCliente from '@/views/ResenasCliente.vue'
 import AboutCliente from '@/views/AboutCliente.vue'
-import homeCliente from '@/views/homeCliente.vue'
+import homeCliente from '@/views/homeCliente.vue'*/
 
 const routes = [
   { path: '/registro', name: 'Registro', component: Registro },
   { path: '/login', name: 'Login', component: Login },
   { path: '/home', name: 'Home', component: Home },
   { path: '/platillos', name: 'Platillos', component: Platillos },
+  { path: '/postres', name: 'Postres', component: Postres },
   { path: '/administracion', name: 'Administrador', component: Administrador},
   { path: '/resenasadmin', name: 'ResenasAdmin', component: ResenasAdmin}, 
   { path: '/aboutadmin', name: 'About', component: About},
